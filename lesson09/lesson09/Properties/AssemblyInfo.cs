9fb8a5eb3366b8200746eb0d5125aa87be09f585
Idea <gulintao850366208@gmail.com>
2017-10-25 19:40:09 +0800

    Debug

--- lesson09/lesson09/Properties/AssemblyInfo.cs
+++ lesson09/lesson09/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("lesson09")]
+[assembly: AssemblyTitle ("Lesson09")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("")]
