4ec8da82ecb81ef4036207d657e5579979ce4417
Idea <gulintao850366208@gmail.com>
2017-10-26 19:39:56 +0800

    till lesson 23

---
+++ Lesson22/Lesson22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson22
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string str;
+			str = "LanOuKeji/Jinwuxing/Qinghe/Haidian/Beijing 2016";
+			string[] strs = str.Split (new char[]{'/'}, 5);
+			foreach (string s in strs) {
+				Console.WriteLine (s);
+			}
+		}
+	}
+}
---
+++ Lesson23/Lesson23/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Lesson23
+{
+	public class Person{
+
+		/* Using method overloads 
+		 * 1. the name should be the same
+		 * 2. the parameters list in the method should not be the same 
+		 * (parametersnumber Or type)
+		 * 
+		 * Note: when in mthod overloads, it's nothing to do with return value
+		*/
+		
+		public int Sum(int a, int b){
+			return a + b;
+		}
+
+		public float Sum(float a, float b){
+			return a + b;
+		}
+
+
+		public int Multiply(int a, int b) {
+			return a * b;
+		}
+
+		public float Multiply(float a, float b) {
+			return a * b;
+		}
+
+		public double Multiply(double a, double b) {
+			return a * b;
+		}
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Person p = new Person ();
+			Console.WriteLine (p.Sum(13, 7));
+
+			Console.WriteLine (p.Sum(3.5f, 7.1f));
+
+			Console.WriteLine (p.Multiply(2, 5));
+			Console.WriteLine (p.Multiply (3.5, 2.7));
+		}
+	}
+}
